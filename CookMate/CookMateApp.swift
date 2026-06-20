7d4dec785ff2ae45528c720064139b6b632ae3e8
Evana Islam <bdevana@gmail.com>
2026-06-20 12:10:11 +1000

    Initial Commit

---
+++ CookMate/CookMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookMateApp.swift
+//  CookMate
+//
+//  Created by Evana Islam on 20/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct CookMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
